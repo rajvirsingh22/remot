f235da8619325b007679546cb33aa4dac5a1e5ec
rajvirsingh22 <rajvirsingh22@ilg.cat>
2022-11-06 00:57:44 +0100

    Fourth Commit

--- src/git01/TestGit.java
+++ src/git01/TestGit.java
@@ -1,8 +1,11 @@
 package git01;
+import java.util.Scanner;
 public class TestGit {
 public static void main(String[] args) {
-String name = "Bugs Bunny";
-System.out.println("Hello " + name.toUpperCase() + "!");
+Scanner scanner = new Scanner(System.in);
+String name = scanner.nextLine();
+System.out.println("Hello " + name + "!");
+scanner.close();
 }
 }
 
