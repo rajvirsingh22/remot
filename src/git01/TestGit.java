95e3143bf3ba7247b78419e8ab5e3ba249c4e699
rajvirsingh22 <rajvirsingh22@ilg.cat>
2022-11-06 01:08:42 +0100

    Fifth Commit

--- src/git01/TestGit.java
+++ src/git01/TestGit.java
@@ -1,13 +1,23 @@
 package git01;
+//<<<<<<< HEAD
 import java.util.Scanner;
+//=======
+//>>>>>>> refs/heads/branch-1
 public class TestGit {
 public static void main(String[] args) {
+//<<<<<<< HEAD
 Scanner scanner = new Scanner(System.in);
 String name = scanner.nextLine();
 System.out.println("Hello " + name + "!");
 scanner.close();
+/*=======
+String name = "Bugs Bunny";
+System.out.println("Hello Mr. " + name.toUpperCase() + "!");
+>>>>>>> refs/heads/branch-1
+*/
 }
 }
+
 
 
 
